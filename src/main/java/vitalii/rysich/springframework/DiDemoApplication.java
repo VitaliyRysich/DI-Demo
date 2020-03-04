2909fefd86ed8dc840b9f432429d5573316653d9
Vitalii Rysich <vitalii.rysich@efl.com.pl>
2020-03-04 13:25:55 +0100

    Init commit

---
+++ src/main/java/vitalii/rysich/springframework/DiDemoApplication.java
@@ -0,0 +1,13 @@
+package vitalii.rysich.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiDemoApplication.class, args);
+    }
+
+}
