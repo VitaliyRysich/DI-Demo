7ef93d507679945def06f3c7fb379ad4335282d4
vitaliyRysich <vitaliy.rysich@gmail.com>
2020-03-19 16:52:11 +0100

    SpringBoot Aplication.properties

--- src/main/java/vitalii/rysich/springframework/config/PropertyConfig.java
+++ src/main/java/vitalii/rysich/springframework/config/PropertyConfig.java
@@ -1,26 +1,14 @@
 package vitalii.rysich.springframework.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 import vitalii.rysich.springframework.examplebeans.FakeDataSource;
 import vitalii.rysich.springframework.examplebeans.FakeJmsBroker;
 
 @Configuration
-//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
-@PropertySources({
-        @PropertySource("classpath:datasource.properties"),
-        @PropertySource("classpath:jms.properties")
-})
 public class PropertyConfig {
-
-    @Autowired
-    Environment env;
 
     @Value("${rysich.username}")
     String user;
@@ -39,7 +27,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(env.getProperty("USERNAME"));
+        fakeDataSource.setUser(user);
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
