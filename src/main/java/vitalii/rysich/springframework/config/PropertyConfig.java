edf26898283e78be175e616d79df7327aaac5f96
vitaliyRysich <vitaliy.rysich@gmail.com>
2020-03-19 16:31:00 +0100

    Adding Spring Environment Properties

--- src/main/java/vitalii/rysich/springframework/config/PropertyConfig.java
+++ src/main/java/vitalii/rysich/springframework/config/PropertyConfig.java
@@ -1,15 +1,21 @@
 package vitalii.rysich.springframework.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+import org.springframework.core.env.Environment;
 import vitalii.rysich.springframework.examplebeans.FakeDataSource;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Autowired
+    Environment env;
 
     @Value("${rysich.username}")
     String user;
@@ -21,7 +27,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(user);
+        fakeDataSource.setUser(env.getProperty("USERNAME"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
